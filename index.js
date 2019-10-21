82d04bd1e6a74d924db7a5a977fea4edde47cf80
inventivetalent <mail@inventivetalent.org>
2019-10-21 08:47:41 +0200

    use googleapis to get proper authentication tokens

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const vars = require("./vars");
 
+const fs = require("fs");
+
 const request = require("request");
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
+
+
+const googleapis = require("googleapis");
+const google = googleapis.google;
+
+const oauth2Client = new google.auth.OAuth2(
+    vars.oauth.id,
+    vars.oauth.secret,
+    vars.oauth.url
+);
+
+let googleAccessTokens;
+
+fs.readFile("googleTokens.json","utf8",(err,data)=>{
+    if (err) {
+        console.warn(err);
+        return;
+    }
+    googleAccessTokens = JSON.parse(data);
+
+    oauth2Client.setCredentials({
+        refresh_token: googleAccessTokens.refresh_token
+    });
+
+    console.log("Previous google tokens loaded");
+});
+
+oauth2Client.on('tokens', (tokens) => {
+    if (tokens.refresh_token) {
+        // store the refresh_token in my database!
+        console.log(tokens.refresh_token);
+        fs.writeFile("googleTokens.json",JSON.stringify(tokens),"utf8",()=>{
+        })
+    }
+    console.log("New Access Token:");
+    console.log(tokens.access_token);
+    googleAccessTokens = token;
+});
 
 // let swStats = require('swagger-stats');
 // app.use(swStats.getMiddleware(vars.swagger));
@@ -27,13 +67,22 @@ app.post("/verifyInAppPurchase/:type/:sub", (req, res) => {
         });
         return;
     }
-    console.log("[VERIFY] " + type + "/" + id + " (" + token + ")");
-    type += "s";
-
     let id = req.params.sub;
     let token = req.body.token;
 
-    request("https://www.googleapis.com/androidpublisher/v3/applications/org.inventivetalent.trashapp/purchases/"+type+"/" + id + "/tokens/" + token, (err, response, body) => {
+    console.log("[VERIFY] " + type + "/" + id + " (" + token + ")");
+    type += "s";
+
+    if (!googleAccessTokens || !googleAccessTokens.access_token) {
+        console.warn("Google tokens not ready");
+        res.status(500).json({
+            success:false,
+            msg:"Google API not ready"
+        });
+        return;
+    }
+
+    request("https://www.googleapis.com/androidpublisher/v3/applications/org.inventivetalent.trashapp/purchases/"+type+"/" + id + "/tokens/" + token+"?access_token="+googleAccessTokens.access_token, (err, response, body) => {
         if (err) {
             console.warn(err);
             res.status(500).json({
@@ -55,7 +104,7 @@ app.post("/verifyInAppPurchase/:type/:sub", (req, res) => {
                 success:true
             })
         }
-    })
+    });
 
 });
 
